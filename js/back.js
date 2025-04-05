4fbca2689a42c0ea4517c1d200bb04e8b50b69ad
ziyunmo <lanzhanfei11@163.com>
2025-04-05 19:42:47 +0800

    Update back.js
    12-9

--- js/back.js
+++ js/back.js
@@ -1,7 +1,7 @@
 function getRandomBackground() {
     const images = [];
     // 假设图片名为 1.jpg 到 10.jpg，按需修改范围
-    for (let i = 1; i <= 12; i++) {
+    for (let i = 1; i <= 9; i++) {
         images.push(`img/${i}.jpg`);
     }
     const randomIndex = Math.floor(Math.random() * images.length);
