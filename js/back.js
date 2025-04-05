3ae250b724df21f77ff2f26e0dc946a61cf69bb1
ziyunmo <lanzhanfei11@163.com>
2025-04-05 19:37:17 +0800

    Add files via upload
    js

---
+++ js/back.js
@@ -0,0 +1,11 @@
+function getRandomBackground() {
+    const images = [];
+    // 假设图片名为 1.jpg 到 10.jpg，按需修改范围
+    for (let i = 1; i <= 12; i++) {
+        images.push(`img/${i}.jpg`);
+    }
+    const randomIndex = Math.floor(Math.random() * images.length);
+    document.body.style.backgroundImage = `url(${images[randomIndex]})`;
+}
+window.onload = getRandomBackground;
+
